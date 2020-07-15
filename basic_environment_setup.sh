a4add411746b8c32d0c6f91bee00c8fb584e89a0
YukinaMochizuki <1p41p4jejo@gmail.com>
2020-07-15 19:16:33 +0800

    Add pacman-contrib to basic install script

--- basic_environment_setup.sh
+++ basic_environment_setup.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-sudo pacman -S --noconfirm git unzip curl wget cmake python3-dev neofetch openssh picom
+sudo pacman -S --noconfirm git unzip curl wget cmake python3-dev neofetch openssh picom pacman-contrib
 
 sudo ln -s /usr/bin/vim /usr/bin/vi
