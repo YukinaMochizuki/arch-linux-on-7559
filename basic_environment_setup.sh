9621156d414e125e8c2b2e4e4d8bef9aba5e4226
YukinaMochizuki <1p41p4jejo@gmail.com>
2020-07-22 23:39:33 +0800

    Add bluetooth support

--- basic_environment_setup.sh
+++ basic_environment_setup.sh
@@ -2,4 +2,11 @@
 
 sudo pacman -S --noconfirm git unzip curl wget cmake python3-dev neofetch openssh picom pacman-contrib
 
+# bluetooth support
+
+sudo pacman -S --noconfirm bluez bluez-utils pulseaudio-bluetooth
+sudo systemctl start bluetooth.servicesudo
+sudo systemctl enable bluetooth.servicesudo
+
+
 sudo ln -s /usr/bin/vim /usr/bin/vi
